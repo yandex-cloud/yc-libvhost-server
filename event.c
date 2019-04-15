11bd0fdae61606574985bcb5f6aa45e0d3d9e4b9
Evgeny Yakovlev <wrfsh@yandex-team.ru>
2019-04-15 21:53:49 +0300

    vhost: rework event loops
    * Allow separate event loop instances to be created
    * Add explicit timeout value in vhd_run_event_loop
    * Add an ability to interrupt running event loop from another thread
    * Provide safe mechanism to terminate running event loop concurrently
    
    Signed-off-by: Evgeny Yakovlev <wrfsh@yandex-team.ru>

--- event.c
+++ event.c
@@ -1,25 +1,52 @@
-// TODO list:
-// * Dynamic event fd vector instead of fixed event array
-// * Timeout argument in run
-// * Thread-safe event source add/remove
-
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdatomic.h>
 #include <sys/epoll.h>
 #include <sys/eventfd.h>
 
 #include "vhost-server/platform.h"
 #include "vhost-server/event.h"
 
-#define VHD_MAX_EVENTS              128
-#define VHD_MAX_EPOLL_TIMEOUT_MS    1000
-#define VHD_EPOLL_EVENTS            (EPOLLIN | EPOLLHUP | EPOLLRDHUP)
+enum event_loop_state
+{
+    EVLOOP_RUNNING = 0,
+    EVLOOP_SHOULD_STOP,
+    EVLOOP_DESTROYED,
+};
 
-/*
- * epoll fd for global event loop
- */
-static int g_event_loop_fd = -1;
+struct vhd_event_loop
+{
+    int epollfd;
+
+    /* eventfd we use to cancel epoll_wait if needed */
+    int interruptfd;
+
+    /* event_loop_state values */
+    atomic_int state;
+
+    /* vhd_terminate_event_loop has been completed */
+    volatile bool is_terminated;
+
+    /* preallocated events buffer */
+    struct epoll_event* events;
+    size_t max_events;
+};
+
+static inline int get_state(struct vhd_event_loop* evloop)
+{
+    return atomic_load(&evloop->state);
+}
+
+static inline bool cas_state(struct vhd_event_loop* evloop, int expected, int desired)
+{
+    return atomic_compare_exchange_strong(&evloop->state, &expected, desired);
+}
+
+static inline bool is_valid(struct vhd_event_loop* evloop)
+{
+    return evloop && (get_state(evloop) != EVLOOP_DESTROYED);
+}
 
 static int handle_one_event(struct vhd_event_ctx* ev, int event_code)
 {
@@ -27,11 +54,13 @@ static int handle_one_event(struct vhd_event_ctx* ev, int event_code)
     VHD_ASSERT(ev->ops);
 
     int ret = 0;
+
+    /* Make sure to handle close events before I/O to avoid reading from broken connection */
     if ((event_code & EPOLLERR) || (event_code & (EPOLLHUP | EPOLLRDHUP))) {
         if (ev->ops->close) {
             ret = ev->ops->close(ev->priv);
         }
-    } else if (event_code == EPOLLIN) {
+    } else if (event_code & EPOLLIN) {
         if (ev->ops->read) {
             ret = ev->ops->read(ev->priv);
         }
@@ -40,10 +69,18 @@ static int handle_one_event(struct vhd_event_ctx* ev, int event_code)
     return ret;
 }
 
-static int handle_events(struct epoll_event *events, int nevents)
+static int handle_events(struct vhd_event_loop* evloop, int nevents)
 {
     int nerr = 0;
+    struct epoll_event* events = evloop->events;
+
     for (int i = 0; i < nevents; i++) {
+        if (events[i].data.fd == evloop->interruptfd) {
+            /* We were interrupted, handle other events normally and ignore this one */
+            vhd_clear_eventfd(evloop->interruptfd);
+            continue;
+        }
+
         struct vhd_event_ctx* ev = events[i].data.ptr;
         if (handle_one_event(ev, events[i].events)) {
             nerr++;
@@ -53,56 +90,156 @@ static int handle_events(struct epoll_event *events, int nevents)
     return nerr;
 }
 
-int vhd_init_event_loop(void)
+struct vhd_event_loop* vhd_create_event_loop(size_t max_events)
 {
-    if (g_event_loop_fd >= 0) {
-        return EBUSY;
-    }
+    int interruptfd = -1;
+    int epollfd = -1;
 
-    int fd = epoll_create1(0);
-    if (fd == -1) {
+    epollfd = epoll_create1(0);
+    if (epollfd < 0) {
         VHD_LOG_ERROR("Can't create epoll fd: %d", errno);
-        return errno;
+        goto error_out;
     }
 
-    g_event_loop_fd = fd;
-    return 0;
+    interruptfd = eventfd(0, EFD_NONBLOCK);
+    if (interruptfd < 0) {
+        VHD_LOG_ERROR("eventfd() failed: %d", errno);
+        goto error_out;
+    }
+
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = interruptfd;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, interruptfd, &ev) == -1) {
+        VHD_LOG_ERROR("Can't add event: %d", errno);
+        goto error_out;
+    }
+
+    struct vhd_event_loop* evloop = vhd_alloc(sizeof(*evloop));
+    evloop->epollfd = epollfd;
+    evloop->interruptfd = interruptfd;
+    evloop->is_terminated = false;
+    evloop->max_events = max_events + 1; /* +1 for interrupt eventfd */
+    evloop->events = vhd_calloc(sizeof(evloop->events[0]), evloop->max_events);
+    evloop->state = EVLOOP_RUNNING;
+
+    return evloop;
+
+error_out:
+    close(interruptfd);
+    close(epollfd);
+    return NULL;
 }
 
-int vhd_run_event_loop(void)
+int vhd_run_event_loop(struct vhd_event_loop* evloop, int timeout_ms)
 {
-    if (!g_event_loop_fd) {
-        return ENXIO;
-    }
+    VHD_VERIFY(is_valid(evloop));
 
-    struct epoll_event events[VHD_MAX_EVENTS];
-
-    int nev = epoll_wait(g_event_loop_fd, events, VHD_MAX_EVENTS, VHD_MAX_EPOLL_TIMEOUT_MS);
-    if (!nev || (nev < 0 && errno == EINTR)) {
+    int nev = epoll_wait(evloop->epollfd, evloop->events, evloop->max_events, timeout_ms);
+    if (!nev) {
         return 0;
     } else if (nev < 0) {
+        if (errno == EINTR || (errno == EBADF && get_state(evloop) == EVLOOP_SHOULD_STOP)) {
+            return 0;
+        }
+
         VHD_LOG_ERROR("epoll_wait internal error: %d", errno);
-        return nev;
+        return -errno;
     }
 
-    int nerr = handle_events(events, nev);
+    int nerr = handle_events(evloop, nev);
     if (nerr) {
         VHD_LOG_WARN("Got %d events, can't handle %d events", nev, nerr);
+        return -EIO;
     }
 
-    return nerr;
+    return nev;
 }
 
-int vhd_add_event(int fd, struct vhd_event_ctx* ctx)
+void vhd_interrupt_event_loop(struct vhd_event_loop* evloop)
 {
+    VHD_VERIFY(is_valid(evloop));
+    vhd_set_eventfd(evloop->interruptfd);
+}
+
+bool vhd_event_loop_terminated(struct vhd_event_loop* evloop)
+{
+    VHD_VERIFY(is_valid(evloop));
+    return evloop->is_terminated;
+}
+
+void vhd_terminate_event_loop(struct vhd_event_loop* evloop)
+{
+    VHD_VERIFY(is_valid(evloop));
+
+    /* Caller terminates event loop from thread 1 and then waits for event loop to exit in thread 2.
+     * After event loop exits in thread 2 caller wants to free it.
+     * There is a race condition with vhd_free_event_loop in this scenario if event loop
+     * exits _before_ this function completes (due to timeout for example). */
+    if (!cas_state(evloop, EVLOOP_RUNNING, EVLOOP_SHOULD_STOP)) {
+        /* Event loop already being freed, don't touch it */
+        return;
+    }
+
+    /* Close epollfd, this way any epoll_wait call will return and fail to restart */
+    close(evloop->epollfd);
+    evloop->is_terminated = true;
+}
+
+void vhd_free_event_loop(struct vhd_event_loop* evloop)
+{
+    if (!evloop) {
+        return;
+    }
+
+    VHD_VERIFY(is_valid(evloop));
+
+    /* See comments about event loop termination race in vhd_terminate_event_loop */
+    if (!cas_state(evloop, EVLOOP_RUNNING, EVLOOP_DESTROYED)) {
+        /* Wait for running termination to complete */
+        while (!evloop->is_terminated)
+            ;
+
+        if (!cas_state(evloop, EVLOOP_SHOULD_STOP, EVLOOP_DESTROYED)) {
+            /* We should only see EVLOOP_SHOULD_STOP state now */
+            VHD_VERIFY(0);
+        }
+    }
+
+    /* epollfd already closed if we were terminated, don't double-close it */
+    if (!evloop->is_terminated) {
+        close(evloop->epollfd);
+    }
+
+    close(evloop->interruptfd);
+    vhd_free(evloop->events);
+    vhd_free(evloop);
+}
+
+int vhd_add_event(struct vhd_event_loop* evloop, int fd, struct vhd_event_ctx* ctx)
+{
+    VHD_VERIFY(is_valid(evloop));
     VHD_VERIFY(ctx && ctx->ops);
 
     struct epoll_event ev;
-    ev.events = VHD_EPOLL_EVENTS;
+    ev.events = EPOLLIN | EPOLLHUP | EPOLLRDHUP;
+    ev.data.fd = fd;
     ev.data.ptr = ctx;
-    if (epoll_ctl(g_event_loop_fd, EPOLL_CTL_ADD, fd, &ev) == -1) {
+    if (epoll_ctl(evloop->epollfd, EPOLL_CTL_ADD, fd, &ev) == -1) {
         VHD_LOG_ERROR("Can't add event: %d", errno);
-        return errno;
+        return -errno;
+    }
+
+    return 0;
+}
+
+int vhd_del_event(struct vhd_event_loop* evloop, int fd)
+{
+    VHD_VERIFY(is_valid(evloop));
+
+    if (epoll_ctl(evloop->epollfd, EPOLL_CTL_DEL, fd, NULL) == -1) {
+        VHD_LOG_ERROR("Can't delete event: %d", errno);
+        return -errno;
     }
 
     return 0;
@@ -116,14 +253,4 @@ void vhd_clear_eventfd(int fd)
 void vhd_set_eventfd(int fd)
 {
     eventfd_write(fd, 1);
-}
-
-int vhd_del_event(int fd)
-{
-    if (epoll_ctl(g_event_loop_fd, EPOLL_CTL_DEL, fd, NULL) == -1) {
-        VHD_LOG_ERROR("Can't delete event: %d", errno);
-        return errno;
-    }
-
-    return 0;
 }
--- include/vhost-server/event.h
+++ include/vhost-server/event.h
@@ -7,6 +7,75 @@ extern "C" {
 #endif
 
 /**
+ * Event loop instance
+ *
+ * Each event loop will run in a thread which calls vhd_run_event_loop.
+ * Events detected in given event loop iteration will also be handled in this thread.
+ *
+ * Event loop management operations (add/remove events) are thread-safe,
+ * although changes to list of events may not be visible until next vhd_run_event_loop.
+ */
+struct vhd_event_loop;
+
+/**
+ * Create new event loop.
+ * @max_events      How many events we can handle in one iteration.
+ *                  Events are reported in FIFO order to avoid starvation.
+ */
+struct vhd_event_loop* vhd_create_event_loop(size_t max_events);
+
+/**
+ * Free event loop.
+ *
+ * May be called when event loop is running,
+ * in which case it will be freed when running iteration ends.
+ */
+void vhd_free_event_loop(struct vhd_event_loop* evloop);
+
+/**
+ * Run event loop with timeout in milliseconds
+ * @timeout     0 to return immediately, -1 to block indefinitely, milliseconds value otherwise.
+ *
+ * @return      Number of events handled in this run or 0 if we have been interrupted or timed out.
+ *              Negative code on error.
+ */
+int vhd_run_event_loop(struct vhd_event_loop* evloop, int timeout_ms);
+
+/**
+ * Kick running event loop out of waiting before timeout expires.
+ * Meant to be called in a thread parallel to vhd_run_event_loop.
+ */
+void vhd_interrupt_event_loop(struct vhd_event_loop* evloop);
+
+/**
+ * Abort event loop running in another thread.
+ *
+ * After calling this function vhd_run_event_loop should (eventually) return and will
+ * always immediately return 0 on subsequent run attempts (but it is still safe to attempt to run).
+ *
+ * vhd_event_loop_terminated will also start to return true.
+ * Assumed caller pattern is to poll termination in one thread and terminate from another, i.e.:
+ *
+ * Thread 1:
+ * --------
+ * while (!vhd_event_loop_terminated(evloop)) {
+ *     vhd_event_loop_run(evloop);
+ * }
+ *
+ * vhd_event_loop_free(evloop);
+ *
+ * Thread 2:
+ * --------
+ * vhd_terminate_event_loop(evloop);
+ */
+void vhd_terminate_event_loop(struct vhd_event_loop* evloop);
+
+/**
+ * Tell if vhd_terminate_event_loop has been called
+ */
+bool vhd_event_loop_terminated(struct vhd_event_loop* evloop);
+
+/**
  * Caller-provided event handler ops
  */
 struct vhd_event_ops
@@ -32,18 +101,18 @@ struct vhd_event_ctx
  * @fd      event fd
  * @ctx     event context description
  */
-int vhd_add_event(int fd, struct vhd_event_ctx* ctx);
+int vhd_add_event(struct vhd_event_loop* evloop, int fd, struct vhd_event_ctx* ctx);
 
 static inline int vhd_make_event(
+    struct vhd_event_loop* evloop,
     int fd,
     void* priv,
     const struct vhd_event_ops* ops,
     struct vhd_event_ctx* ctx)
 {
-    VHD_VERIFY(ctx);
     ctx->priv = priv;
     ctx->ops = ops;
-    return vhd_add_event(fd, ctx);
+    return vhd_add_event(evloop, fd, ctx);
 }
 
 /**
@@ -59,17 +128,7 @@ void vhd_set_eventfd(int fd);
 /**
  * Remove event from event loop
  */
-int vhd_del_event(int fd);
-
-/**
- * Init global default event loop
- */
-int vhd_init_event_loop(void);
-
-/**
- * Run a blocking iteration of default event loop in calling thread.
- */
-int vhd_run_event_loop(void);
+int vhd_del_event(struct vhd_event_loop* evloop, int fd);
 
 #ifdef __cplusplus
 }
