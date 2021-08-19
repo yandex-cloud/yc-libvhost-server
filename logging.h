18e5e1ed347f3d1422d9d2253bf4a8ed4616c99c
Roman Kagan <rvkagan@yandex-team.ru>
2021-08-20 01:06:20 +0300

    logging: add helpers for object-tagged log messages
    In the situation where the vhost server handles multiple devices, with
    multiple queues each, it becomes hard to tell which particular object
    (among the multiple ones of the same type) a message pertains to.
    
    To make it easier yet generic enough to be usable with all types of
    objects, implement another set of logging macros, taking additionally a
    pointer to the object the message pertains to.  The object is assumed to
    have a const string field ->log_tag which is then prepended to the
    message.
    
    The patches introducing the use of these macros will follow.
    
    Signed-off-by: Roman Kagan <rvkagan@yandex-team.ru>

--- logging.h
+++ logging.h
@@ -21,7 +21,20 @@ extern log_function __attribute__((format(printf, 2, 3))) g_log_fn;
 #endif
 
 #define VHD_LOG_INFO(fmt, ...)     VHD_LOG(LOG_INFO, fmt, ##__VA_ARGS__)
-
 #define VHD_LOG_WARN(fmt, ...)     VHD_LOG(LOG_WARNING, fmt, ##__VA_ARGS__)
-
 #define VHD_LOG_ERROR(fmt, ...)    VHD_LOG(LOG_ERROR, fmt, ##__VA_ARGS__)
+
+/*
+ * Generic helpers to produce log messages tagged by an object.  For that, the
+ * object must provide duck-typed interface of ->log_tag field of type "const
+ * char *".
+ */
+#define VHD_OBJ_DEBUG(obj, fmt, ...) \
+    VHD_LOG_DEBUG("%s: " fmt, obj->log_tag, ##__VA_ARGS__)
+#define VHD_OBJ_INFO(obj, fmt, ...) \
+    VHD_LOG_INFO("%s: " fmt, obj->log_tag, ##__VA_ARGS__)
+#define VHD_OBJ_WARN(obj, fmt, ...) \
+    VHD_LOG_WARN("%s: " fmt, obj->log_tag, ##__VA_ARGS__)
+#define VHD_OBJ_ERROR(obj, fmt, ...) \
+    VHD_LOG_ERROR("%s: " fmt, obj->log_tag, ##__VA_ARGS__)
+
