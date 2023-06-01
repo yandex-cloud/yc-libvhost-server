67e9767e580b6ae8d3d21b9f2eb8660641e10618
Anton Kuchin <antonkuchin@yandex-team.ru>
2023-06-01 13:34:57 +0300

    metrics: reorder struct members to group related counters
    Now dispatch and request metrics are in more logical order.
    First go dispatch counters that describe number of processing
    events and then request counters.
    
    Signed-off-by: Anton Kuchin <antonkuchin@yandex-team.ru>

--- include/vhost/types.h
+++ include/vhost/types.h
@@ -29,18 +29,19 @@ struct vhd_sglist {
  * virtqueue usage statistics
  */
 struct vhd_vq_metrics {
-    /* total amount of requests processed */
-    uint64_t request_total;
-
+    /* Dispatch counters */
     /* number of times vring was processed */
     uint64_t dispatch_total;
-
     /* number of times vring was empty on processing */
     uint64_t dispatch_empty;
 
+    /* Request counters */
+    /* total amount of requests processed */
+    uint64_t request_total;
+
+    /* Other counters*/
     /* number of requests was dispatched from vring last time*/
     uint16_t queue_len_last;
-
     /* max queue len was processed during 60s period */
     uint16_t queue_len_max_60s;
 };
